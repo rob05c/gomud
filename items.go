eb9dfb4dc7b339a2782d5663dc5fe0e74549b84e
robert.o.butts <devnull@localhost>
2013-04-16 23:27:46 -0500

    issue 5 added itemManager

---
+++ items.go
@@ -0,0 +1,112 @@
+package main
+
+type itemIdentifier identifier
+
+type item struct {
+	id     itemIdentifier
+	name   string
+	brief  string
+	long   string
+	ground string
+}
+
+type itemManager struct {
+	request chan struct {
+		id       itemIdentifier
+		response chan struct {
+			item
+			bool
+		}
+	}
+	change chan struct {
+		id     itemIdentifier
+		modify func(*item)
+	}
+	create chan struct {
+		newItem  item
+		response chan itemIdentifier
+	}
+}
+
+func (m itemManager) getItem(itemId itemIdentifier) (newItem item, exists bool) {
+	responseChan := make(chan struct {
+		item
+		bool
+	})
+	m.request <- struct {
+		id       itemIdentifier
+		response chan struct {
+			item
+			bool
+		}
+	}{itemId, responseChan}
+	response := <-responseChan
+	return response.item, response.bool
+}
+
+func (m itemManager) createItem(i item) itemIdentifier {
+	responseChan := make(chan itemIdentifier)
+	m.create <- struct {
+		newItem  item
+		response chan itemIdentifier
+	}{i, responseChan}
+	newItemId := <-responseChan
+	return newItemId
+}
+
+/// callers should be aware this is asynchronous - the room is not necessarily changed immediately upon return
+/// anything in modify besides modifying the room MUST be called in a goroutine. Else, deadlock.
+func (m itemManager) changeItem(id itemIdentifier, modify func(*item)) {
+	m.change <- struct {
+		id     itemIdentifier
+		modify func(*item)
+	}{id, modify}
+}
+
+func newItemManager() *itemManager {
+	itemManager := &itemManager{request: make(chan struct {
+		id       itemIdentifier
+		response chan struct {
+			item
+			bool
+		}
+	}), change: make(chan struct {
+		id     itemIdentifier
+		modify func(*item)
+	}), create: make(chan struct {
+		newItem  item
+		response chan itemIdentifier
+	})}
+	go manageItems(itemManager)
+	return itemManager
+}
+
+func manageItems(manager *itemManager) {
+	var items = map[itemIdentifier]*item{}
+	for {
+		select {
+		case r := <-manager.request:
+			i, exists := items[r.id]
+			var itemCopy item
+			if exists {
+				itemCopy = *i
+			} else {
+				itemCopy = item{id: -1}
+			}
+			r.response <- struct {
+				item
+				bool
+			}{itemCopy, exists}
+		case h := <-manager.change:
+			i, exists := items[h.id]
+			if !exists {
+				continue
+			}
+			h.modify(i)
+		case c := <-manager.create:
+			c.newItem.id = itemIdentifier(len(items))
+			items[c.newItem.id] = &c.newItem
+			c.response <- c.newItem.id
+		}
+	}
+}
--- mud.go
+++ mud.go
@@ -4,9 +4,11 @@ const defaultPort = 9241
 
 type identifier int32
 
+// @todo rename managers to world.rooms, world.playerLocations, world.players.
 type metaManager struct {
 	*playerManager
 	*roomManager
+	*itemManager
 	*playerLocationManager
 }
 
@@ -14,6 +16,7 @@ func initialize() metaManager {
 	initCommands()
 	playerManager := newPlayerManager()
 	roomManager := newRoomManager()
+	itemManager := newItemManager()
 	playerLocationManager := newPlayerLocationManager(roomManager)
 	roomManager.createRoom(room{
 		id:          roomIdentifier(0),
@@ -21,7 +24,7 @@ func initialize() metaManager {
 		description: "Everything has a beginning. This is only one of many beginnings you will soon find as I continue typing in order to create a wall of text to test this. It's a very long sentence that precedes this slightly shorter one. Blarglblargl.",
 		exits:       make(map[Direction]roomIdentifier),
 	})
-	return metaManager{playerManager, roomManager, playerLocationManager}
+	return metaManager{playerManager, roomManager, itemManager, playerLocationManager}
 }
 
 func main() {
