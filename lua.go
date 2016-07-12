726f669ab70001eb424fab8ef85a2c7dd8dd5837
Robert Butts <robert.o.butts@gmail.com>
2016-07-12 10:56:07 -0600

    Add luaRevalFunc

--- lua.go
+++ lua.go
@@ -4,23 +4,15 @@ import (
 	"fmt"
 	"github.com/Shopify/go-lua"
 	"strconv"
+	"time"
 )
-
-// luaNpcFunc is an NPC function, which requires the world and the NPC identifier
-type luaNpcFunc func(l *lua.State, world *World, npcId identifier) int
-
-// wrapLuaFunc takes a luaNpcFunc and closes and wraps it into a lua.Function
-func wrapLuaFunc(f luaNpcFunc, world *World, npcId identifier) lua.Function {
-	return func(l *lua.State) int {
-		return f(l, world, npcId)
-	}
-}
 
 // funcs returns a map of lua.Functions to be registered, creating closures with the world and npc.
 func funcs(world *World, npcId identifier) map[string]lua.Function {
 	return map[string]lua.Function{
 		"gomud_println":     luaPrintln,
-		"gomud_roomPlayers": wrapLuaFunc(luaGetRoomPlayers, world, npcId),
+		"gomud_roomPlayers": luaGetRoomPlayersFunc(world, npcId),
+		"gomud_reval":       luaRevalFunc(world, npcId),
 	}
 }
 
@@ -32,6 +24,51 @@ func initLua(world *World, id identifier) *lua.State {
 		l.Register(name, f)
 	}
 	return l
+}
+
+// luaRevalFunc takes a wait time in milliseconds from the script,
+// and re-executes the npc.Animate after the wait time.
+// Scripts should call this at the end of their animation right before returning.
+// TODO add a min and max cap, possibly allowing admins to create NPCs under the min cap.
+// TODO find a way to prevent users calling reval() and not returning (because that would have 2 threads accessing lua.State, and it's not threadsafe)
+// example test lua:
+// gomud_println("bill is here. You should feel honoured."); gomud_reval(1000)
+func luaRevalFunc(world *World, npcId identifier) lua.Function {
+	return func(l *lua.State) int {
+		n := l.Top() // Number of arguments.
+		if n != 1 {
+			l.PushString("incorrect number of arguments: expected 1 got " + strconv.Itoa(n))
+			l.Error() // panics
+		}
+
+		waitMs, ok := l.ToInteger(1)
+		if !ok {
+			l.PushString("incorrect argument type: expected integer")
+			l.Error() // panics
+		}
+
+		go func() {
+			time.Sleep(time.Duration(waitMs) * time.Millisecond)
+			self, ok := world.npcs.GetById(npcId)
+			if !ok {
+				fmt.Println("npcReval npc nonexistent '" + npcId.String() + "'")
+				return
+			}
+			self.Animate(world)
+		}()
+
+		return 0
+	}
+}
+
+// luaPushStrings pushes an array (table) to the stack (function return)
+func luaPushStrings(l *lua.State, ss []string) {
+	l.NewTable()
+	for i, s := range ss {
+		l.PushInteger(i)
+		l.PushString(s)
+		l.SetTable(-3)
+	}
 }
 
 // luaPrintln prints a string to the Server console. Used for server debugging. NPC scripters should not call this, and it should not be in the user-facing help.
@@ -55,49 +92,41 @@ func luaPrintln(l *lua.State) int {
 	return 0 // Result count
 }
 
-// luaPushStrings pushes an array (table) to the stack (function return)
-func luaPushStrings(l *lua.State, ss []string) {
-	l.NewTable()
-	for i, s := range ss {
-		l.PushInteger(i)
-		l.PushString(s)
-		l.SetTable(-3)
-	}
-}
-
 // luaGetRoomPlayers returns/pushes an array/table of the players in the room with the given npc
 // example test lua:
 // players = gomud_roomPlayers(); for i,player in pairs(players) do gomud_println(player) end
-func luaGetRoomPlayers(l *lua.State, world *World, npcId identifier) int {
-	self, ok := world.npcs.GetById(npcId)
-	if !ok {
-		fmt.Println("luaGetRoomPlayers got nonlocated npc '" + npcId.String() + "'")
-		l.PushString("error: self not found")
-		l.Error() // panics
-	}
-
-	if self.LocationType != ilRoom {
-		fmt.Println("luaGetRoomPlayers got npc in nonroom '" + npcId.String() + "'")
-		l.PushString("error: self in nonroom")
-		l.Error() // panics
-	}
-
-	room, ok := world.rooms.GetById(self.Location)
-	if self.LocationType != ilRoom {
-		fmt.Println("luaGetRoomPlayers npc room not room '" + npcId.String() + "'")
-		l.PushString("error: self not in room")
-		l.Error() // panics
-	}
-
-	var players []string
-	for id, _ := range room.Players {
-		player, ok := world.players.GetById(id)
+func luaGetRoomPlayersFunc(world *World, npcId identifier) lua.Function {
+	return func(l *lua.State) int {
+		self, ok := world.npcs.GetById(npcId)
 		if !ok {
-			continue
+			fmt.Println("luaGetRoomPlayers got nonlocated npc '" + npcId.String() + "'")
+			l.PushString("error: self not found")
+			l.Error() // panics
 		}
-		players = append(players, player.Name())
-	}
 
-	luaPushStrings(l, players)
-	return 1
+		if self.LocationType != ilRoom {
+			fmt.Println("luaGetRoomPlayers got npc in nonroom '" + npcId.String() + "'")
+			l.PushString("error: self in nonroom")
+			l.Error() // panics
+		}
+
+		room, ok := world.rooms.GetById(self.Location)
+		if self.LocationType != ilRoom {
+			fmt.Println("luaGetRoomPlayers npc room not room '" + npcId.String() + "'")
+			l.PushString("error: self not in room")
+			l.Error() // panics
+		}
+
+		var players []string
+		for id, _ := range room.Players {
+			player, ok := world.players.GetById(id)
+			if !ok {
+				continue
+			}
+			players = append(players, player.Name())
+		}
+
+		luaPushStrings(l, players)
+		return 1
+	}
 }
